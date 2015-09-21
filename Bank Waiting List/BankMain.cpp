c8ea5fa345c1444a8bbb8ebe1ccf94369d15de42
Neolus <swillow233@gmail.com>
2015-09-22 01:19:56 +0800

    Update BankMain.cpp

--- Bank Waiting List/BankMain.cpp
+++ Bank Waiting List/BankMain.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include "Bank.h"
 
-int Bank::Worker::increaseWorker=0;
+int Bank::Worker::increaseWorker=0; //Set static values.
 int Bank::Customer::increaseCus=0;
 using namespace std;
 
@@ -11,8 +11,8 @@ void main()
 {
 	
 	Bank CCB(6);
-	for(int i=0;i!=96;++i)
-	CCB.update();
+	for(int i=0;i!=96;++i) //Whole the day equals 96 unit times.
+        CCB.update();          //All information are placed in CCB.
 
 	system("pause");
 
